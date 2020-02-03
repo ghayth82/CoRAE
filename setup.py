96575c53f61f07135d93c198728c259300a61d23
pwaabdullah <aamcse@gmail.com>
2020-02-03 17:42:45 -0500

    setup file created

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as txt:
+    full_description = txt.read()
+
+setuptools.setup(
+    name="CoRAE",
+    version="1.0",
+    author="Abdullah Al Mamun",
+    author_email="aamcse@gmail.com",
+    description="A feature selection framwork",
+    long_description=full_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pwaabdullah/CoRAE",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=['keras'],
+    python_requires='>=3',
+)
